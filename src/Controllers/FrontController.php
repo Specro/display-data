44e416b691911d3b44bdd75180b9b4de7b90ee7e
Liudas Dzisevicius <liudas.dzisevicius@gmail.com>
2025-01-22 19:27:02 +0200

    Add file handling and validation methods to FrontController

--- src/Controllers/FrontController.php
+++ src/Controllers/FrontController.php
@@ -22,10 +22,73 @@ class FrontController extends Controller
     {
         $users = [];
 
+        if (isset($_FILES['file'])) {
+            $users = $this->handleFile($_FILES['file']);
+        }
+
         $this->view->render([
             'users' => $users,
             'allowed_file_types' => implode(',', array_keys($this->config['file_types'])),
             'errors' => $this->errors
         ]);
+    }
+    
+    /**
+     * Handle submitted file.
+     * @param mixed $file
+     * @return array
+     */
+    protected function handleFile($file)
+    {
+        $users = [];
+
+        try {
+            $this->validateFile($file);
+            $users = $this->readFile($file);
+        } catch (\Throwable $th) {
+            $this->errors[] = $th->getMessage();
+        }
+
+        return $users;
+    }
+
+    /**
+     * Validate and handle any errors that happen during file upload.
+     * @param mixed $file
+     * @throws \RuntimeException
+     * @return void
+     */
+    protected function validateFile($file)
+    {
+        if (!isset($file['error']) || is_array($file['error'])) {
+            throw new \RuntimeException('Invalid parameters');
+        }
+
+        switch ($file['error']) {
+            case UPLOAD_ERR_OK:
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                throw new \RuntimeException('No file sent');
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                throw new \RuntimeException('File size exceeded');
+            default:
+                throw new \RuntimeException('Something went wrong');
+        }
+
+        $file_type = mime_content_type($file['tmp_name']);
+        if (!isset($this->allowed_files[$file_type])) {
+            throw new \RuntimeException('Invalid file format');
+        }
+    }
+
+    /**
+     * Read the file and return data models.
+     * @param mixed $file
+     * @return array
+     */
+    protected function readFile($file)
+    {
+        return [];
     }
 }
