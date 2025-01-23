037bf423a477ec93b8dd7d34d8a148a1b74f1f5f
Liudas Dzisevicius <liudas.dzisevicius@gmail.com>
2025-01-23 10:58:27 +0200

    Add configuration comments

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
 $config = [
+    // map mime types to file reader classes
     'file_types' => [
         'application/json' => 'JSONReader',
         'application/xml' => 'XMLReader',
@@ -8,5 +9,6 @@ $config = [
         'application/csv' => 'CSVReader',
         'text/csv' => 'CSVReader'
     ],
+    // base path for php view templates
     'base_template_path' => __DIR__.'/templates'
 ];
