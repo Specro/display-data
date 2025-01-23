dccbf8cbf578caf5dd68edebd1248aef2b4d1e43
Liudas Dzisevicius <liudas.dzisevicius@gmail.com>
2025-01-23 10:48:53 +0200

    Add front layout styles

--- templates/front-layout.php
+++ templates/front-layout.php
@@ -4,31 +4,43 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Data Display</title>
+        <link rel="stylesheet" href="assets/css/main.css">
     </head>
     <body>
-        <form method="post" enctype="multipart/form-data">
-            <label for="file-input">File input</label>
-            <input type="file" id="file-input" name="file" accept="<?= $allowed_file_types; ?>" />
-            <button type="submit">Submit</button>
-        </form>
-        <?php if ($errors): ?>
-            <div>
-            <?php foreach ($errors as $error): ?>
-                <p><?= $error; ?></p>
-            <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
-        <?php if (!empty($users)): ?>
-        <table>
-            <tr></tr>
-            <?php foreach ($users as $user): ?>
-                <tr>
-                    <td><?= $user->getFirstName(); ?></td>
-                    <td><?= $user->getAge(); ?></td>
-                    <td><?= $user->getGender(); ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        <?php endif; ?>
+        <main>
+            <form method="post" enctype="multipart/form-data">
+                <label for="file-input">File</label>
+                <p class="explanation">Upload a user file to display it's data.</p>
+                <input type="file" id="file-input" name="file" accept="<?= $allowed_file_types; ?>" />
+                <button type="submit">Submit</button>
+            </form>
+            <?php if ($errors): ?>
+                <div class="notification notification--error">
+                <?php foreach ($errors as $error): ?>
+                    <p><?= $error; ?></p>
+                <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($users)): ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Age</th>
+                        <th>Gender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($users as $user): ?>
+                        <tr>
+                            <td><?= $user->getFirstName(); ?></td>
+                            <td><?= $user->getAge(); ?></td>
+                            <td><?= $user->getGender(); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            <?php endif; ?>
+        </main>
     </body>
 </html>
